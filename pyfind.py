6a60044d469bb670854ec0b53155cc085a340a5d
Ryan Krauss <ryanlists@gmail.com>
2009-07-02 10:02:28 -0500

    Add md5sum and one or two other files

---
+++ pyfind.py
@@ -0,0 +1,19 @@
+import glob, os
+
+from  IPython.Debugger import Pdb
+
+def findall(topdir, pat, walk=True, includetop=1):
+    if includetop:
+        curpat = os.path.join(topdir, pat)
+        myfiles = glob.glob(curpat)
+    else:
+        myfiles = []
+    if walk:
+        for root, dirs, files in os.walk(topdir):
+            for curdir in dirs:
+                curpath = os.path.join(root,curdir)
+                curpat = os.path.join(curpath, pat)
+                curfiles = glob.glob(curpat)
+                myfiles.extend(curfiles)
+    return myfiles
+            
