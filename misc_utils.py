42d437d246b39ec98fbd03484ecf3784a33f1c0b
Ryan Krauss <ryanlists@gmail.com>
2010-01-19 16:22:16 -0600

    pulled some _dump_vectors logic out of dump_vectors to create a separate utility function.

--- misc_utils.py
+++ misc_utils.py
@@ -4,6 +4,12 @@ import os
 
 lf = os.linesep#determing the linefeed for the operating system ('\n' for Linux or '\r\n' for Windows)
 
+def _dump_vectors(f, vectorlist, fmt='%0.10g', delim='\t'):
+    mymatrix = column_stack(vectorlist)#create a matrix with each vector as a column
+    savetxt(f, mymatrix, fmt=fmt, delimiter=delim)
+    return f
+    
+
 def dump_vectors(filename, vectorlist, labels, fmt='%0.10g', delim='\t'):
     """Dump a list of vectors to a text file where each vector is a
     columnm in a spreadsheet style text file.
@@ -19,10 +25,9 @@ def dump_vectors(filename, vectorlist, labels, fmt='%0.10g', delim='\t'):
 
     delim is the delimiter to use between columns of the file.  The
     default is a tab."""
-    mymatrix = column_stack(vectorlist)#create a matrix with each vector as a column
     f = open(filename, 'w')#open a file for writing.  Caution: this will overwrite an existing file
     firstrow = delim.join(labels)#create a delimited first row of the labels
     f.write(firstrow+'\n')#write the first row and a line feed to the file
-    savetxt(f, mymatrix, fmt=fmt, delimiter=delim)
+    _dump_vectors(f, vectorlist, fmt=fmt, delim=delim)
     f.close()#close the file
     
