8a007fdf6ff36e9dbac7c7655ea37091809246f1
Ryan Krauss <ryanlists@gmail.com>
2012-06-02 10:12:56 -0500

    added dump_matrix capability to misc_utils.py

--- misc_utils.py
+++ misc_utils.py
@@ -4,12 +4,25 @@ import os
 
 lf = os.linesep#determing the linefeed for the operating system ('\n' for Linux or '\r\n' for Windows)
 
-def _dump_vectors(f, vectorlist, fmt='%0.10g', delim='\t'):
-    mymatrix = column_stack(vectorlist)#create a matrix with each vector as a column
-    savetxt(f, mymatrix, fmt=fmt, delimiter=delim)
+def _dump_matrix(f, matrix, fmt='%0.10g', delim='\t'):
+    savetxt(f, matrix, fmt=fmt, delimiter=delim)
     return f
     
+def _dump_vectors(f, vectorlist, fmt='%0.10g', delim='\t'):
+    mymatrix = column_stack(vectorlist)#create a matrix with each vector as a column
+    _dump_matrix(f, mymatrix, fmt=fmt, delim=delim)
 
+
+def _open_file(filename):
+    f = open(filename, 'w')#open a file for writing.  Caution: this will overwrite an existing file
+    return f
+
+
+def _save_labels(f, labels, delim='\t'):
+    firstrow = delim.join(labels)#create a delimited first row of the labels
+    f.write(firstrow+'\n')#write the first row and a line feed to the file
+    return f
+    
 def dump_vectors(filename, vectorlist, labels, fmt='%0.10g', delim='\t'):
     """Dump a list of vectors to a text file where each vector is a
     columnm in a spreadsheet style text file.
@@ -25,9 +38,18 @@ def dump_vectors(filename, vectorlist, labels, fmt='%0.10g', delim='\t'):
 
     delim is the delimiter to use between columns of the file.  The
     default is a tab."""
-    f = open(filename, 'w')#open a file for writing.  Caution: this will overwrite an existing file
-    firstrow = delim.join(labels)#create a delimited first row of the labels
-    f.write(firstrow+'\n')#write the first row and a line feed to the file
+    f = _open_file(filename)
+    _save_labels(f, labels, delim=delim)
     _dump_vectors(f, vectorlist, fmt=fmt, delim=delim)
     f.close()#close the file
+
     
+def dump_matrix(filename, matrix, labels, fmt='%0.10g', delim='\t'):
+    """Similar to dump_vectors, but the data is already a matrix that
+    needs to be dumped to a file with a lable row."""
+    f = _open_file(filename)
+    _save_labels(f, labels, delim=delim)
+    _dump_matrix(f, matrix, fmt=fmt, delim=delim)
+    f.close()#close the file
+
+
