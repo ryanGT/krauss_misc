69810406bdb87e6a9778c1d50fc4ddb7bcb7031a
Ryan Krauss <ryanwkrauss@gmail.com>
2016-02-21 21:12:39 -0600

    getting ready for first rsync tests

--- rsync_utils.py
+++ rsync_utils.py
@@ -4,6 +4,8 @@ filesep = os.path.sep
 
 src_base = '/Users/rkrauss'
 
+exclude_from_str = '--exclude-from=/Users/rkrauss/scripts/rsync_excludes.txt'
+
 def check_src(src_in):
     assert src_in.find(src_base) == 0, 'bad src: %s' % src_in
     
@@ -11,22 +13,25 @@ def check_dst(dst_in):
     assert dst_in.find('/Volumes/') == 0, 'bad dst: %s' % dst_in
     
     
-def rsync_one(src, dst, flags=['-av']):
+def rsync_one(src, dst, flags=['-av',exclude_from_str]):
     check_src(src)
     check_dst(dst)
     flag_str = ' '.join(flags)
     cmd = 'rsync %s %s %s' % (src, dst, flags)
-    os.system(cmd
+    print(cmd)
+    #os.system(cmd)
 
               
 class rsync_runner(object):
-    def __init__(self, src_root, dst_root, dst_folder='', relpaths=[]):
+    def __init__(self, src_root, dst_root, dst_folder='', relpaths=[], \
+                 flags=['-av',exclude_from_str]):
         self.src_root = os.path.expanduser(src_root)
         check_src(self.src_root)
         self.dst_root = dst_root
         check_dst(self.dst_root)
         self.dst_folder = dst_folder
         self.relpaths = relpaths
+        self.flags = flags
 
 
     def make_dst_dirs(self):
@@ -57,3 +62,13 @@ class rsync_runner(object):
             if not os.path.exists(cur_dest):
                 rwkos.make_dirs_recrusive(cur_dest)
             
+
+
+    def run_rsync(self):
+        if not hasattr(self, 'dst_path'):
+            self.make_dst_dirs()
+            
+        for item in self.relpaths:
+            src_path = os.path.join(self.src_root, item)
+            cur_dest = os.path.join(self.dst_path, item)
+            rsync_one(src_path, cur_dest, flags=self.flags)
